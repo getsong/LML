90e308a7745f98ccc45bd625b98bf692c6968357
personal <getsong13@gmail.com>
2019-04-13 23:46:52 +0800

    initialize spring boot project

---
+++ src/main/java/com/getsong/lml/LmlApplication.java
@@ -0,0 +1,13 @@
+package com.getsong.lml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmlApplication.class, args);
+	}
+
+}
