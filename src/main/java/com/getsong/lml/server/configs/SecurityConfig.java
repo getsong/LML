2342e53e65458081cadbd454f784066726488404
personal <getsong13@gmail.com>
2019-04-21 16:49:16 +0800

    add http basic auth with in memory auth

---
+++ src/main/java/com/getsong/lml/server/configs/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.getsong.lml.server.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Security configuration for the entire server application using spring security.
+ *
+ * @author getsong
+ * @since 19/4/2019 5:08 PM
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(encoder().encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("user").password(encoder().encode("user")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+//                .antMatchers("/books").hasRole("ADMIN")
+                .antMatchers("/**").authenticated()
+                .and()
+                .formLogin().disable();
+    }
+
+}
