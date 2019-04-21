2970a368592caf6895913e22eb60e537d34f0e17
personal <getsong13@gmail.com>
2019-04-22 00:33:16 +0800

    disable csrf in order to test api in Postman

--- src/main/java/com/getsong/lml/server/configs/SecurityConfig.java
+++ src/main/java/com/getsong/lml/server/configs/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.getsong.lml.server.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,10 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+        http.csrf().disable()
+                .httpBasic()
                 .and()
                 .authorizeRequests()
-//                .antMatchers("/books").hasRole("ADMIN")
                 .antMatchers("/**").authenticated()
                 .and()
                 .formLogin().disable();
