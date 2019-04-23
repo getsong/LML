67c7cb34b440f05f07c89a89fe2b0915e4bc83df
personal <getsong13@gmail.com>
2019-04-23 22:36:13 +0800

    Use GenerationType.IDENTITY to make id increment from its own last value

--- src/main/java/com/getsong/lml/server/models/Book.java
+++ src/main/java/com/getsong/lml/server/models/Book.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String title;
--- src/main/java/com/getsong/lml/server/models/Library.java
+++ src/main/java/com/getsong/lml/server/models/Library.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class Library {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
